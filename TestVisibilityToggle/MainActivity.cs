4fb67aadb7a71ea76c90313fcd65eaad197b6e9a
Raimo Huybrechts <raimo.huybrechts@flowpilots.com>
2017-04-14 10:27:52 +0200

    first commit

---
+++ TestVisibilityToggle/MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+
+namespace TestVisibilityToggle
+{
+    [Activity(Label = "TestVisibilityToggle", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView (Resource.Layout.Main);
+        }
+    }
+}
+
